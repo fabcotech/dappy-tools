09b03be7877218b4216b0660ad082dc78c32ee3c
Paul Musso <paul@musso.io>
2022-03-28 10:10:29 +0200

    refacto: extract decodeCertificate from getCertificates

--- src/getCertificates.ts
+++ src/getCertificates.ts
@@ -1,15 +1,25 @@
 import { createCoResolveQuery } from './coResolveQuery';
 import { createPostJSONQuery } from './queries/postJSONQuery';
-import { DappyLookupOptions } from './types';
+import { DappyLookupOptions, NamePacket } from './types';
 import { nodeRequest } from './utils/nodeRequest';
 
 export const CERT_QUERY_PATH = '/get-certificates';
+
+export const decodeCertificates = (packet: NamePacket) => {
+  return {
+    ...packet,
+    answers: packet.answers.map((a) => ({
+      ...a,
+      data: Buffer.from(a.data, 'base64').toString('utf8'),
+    })),
+  };
+};
 
 export const getCertificates = async (
   name: string,
   options?: DappyLookupOptions,
 ) => {
-  const r = await createCoResolveQuery(
+  const packet = await createCoResolveQuery(
     createPostJSONQuery(nodeRequest, { path: CERT_QUERY_PATH }),
   )(
     {
@@ -17,11 +27,5 @@ export const getCertificates = async (
     },
     options,
   );
-  return {
-    ...r,
-    answers: r.answers.map((a) => ({
-      ...a,
-      data: Buffer.from(a.data, 'base64').toString('utf8'),
-    })),
-  };
+  return decodeCertificates(packet);
 };
