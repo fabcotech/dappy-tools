a1fdcf09c3ae8031c089fb2797b3dbb83aeb93e9
Paul Musso <paul@musso.io>
2022-03-29 08:22:34 +0200

    add: save certificate custom out path

--- src/cli/commands/saveCertificateCommand.spec.ts
+++ src/cli/commands/saveCertificateCommand.spec.ts
@@ -65,6 +65,61 @@ describe('cli command: savecertificate', () => {
     );
     expect(r).to.eql(0);
   });
+  it('save 1 certificate as file with --out', async () => {
+    const fakeWriteFile = chai.spy(() => Promise.resolve());
+    const r = await saveCertificateCommand.action(
+      ['example', '--out=toto.pem'],
+      {
+        lookup: chai.spy(() =>
+          Promise.resolve(createCertNamePacketSuccessResponse()),
+        ),
+        print: chai.spy(),
+        readFile: chai.spy(() => Promise.resolve('')),
+        writeFile: fakeWriteFile,
+      },
+    );
+    expect(fakeWriteFile).to.have.been.called.with('toto.pem', fakeCertificate);
+    expect(r).to.eql(0);
+  });
+  it('save 2 certificates as files with --out', async () => {
+    const fakeWriteFile = chai.spy(() => Promise.resolve());
+    const r = await saveCertificateCommand.action(['example', '--out=foo'], {
+      lookup: chai.spy(() =>
+        Promise.resolve(
+          createCertNamePacketSuccessResponse({
+            answers: [
+              {
+                name: 'example',
+                type: RecordType.CERT,
+                class: 'IN',
+                ttl: 60,
+                data: fakeCertificate,
+              },
+              {
+                name: 'example',
+                type: RecordType.CERT,
+                class: 'IN',
+                ttl: 60,
+                data: fakeCertificate,
+              },
+            ],
+          }),
+        ),
+      ),
+      print: chai.spy(),
+      readFile: chai.spy(() => Promise.resolve('')),
+      writeFile: fakeWriteFile,
+    });
+    expect(fakeWriteFile).to.have.been.first.called.with(
+      'foo-1.pem',
+      fakeCertificate,
+    );
+    expect(fakeWriteFile).to.have.been.second.called.with(
+      'foo-2.pem',
+      fakeCertificate,
+    );
+    expect(r).to.eql(0);
+  });
   it('missing name', async () => {
     const fakePrint = chai.spy();
     const r = await saveCertificateCommand.action([], {
@@ -78,5 +133,23 @@ describe('cli command: savecertificate', () => {
 
     expect(r).to.eql(1);
     expect(fakePrint).to.have.been.called.with('missing name');
+  });
+  it('no answer', async () => {
+    const fakeWriteFile = chai.spy(() => Promise.resolve());
+    const print = chai.spy();
+    const r = await saveCertificateCommand.action(['example'], {
+      lookup: chai.spy(() =>
+        Promise.resolve(
+          createCertNamePacketSuccessResponse({
+            answers: [],
+          }),
+        ),
+      ),
+      print,
+      readFile: chai.spy(() => Promise.resolve('')),
+      writeFile: fakeWriteFile,
+    });
+    expect(print).to.have.been.called.with('Certificate(s) example not found');
+    expect(r).to.eql(1);
   });
 });
--- src/cli/commands/saveCertificateCommand.ts
+++ src/cli/commands/saveCertificateCommand.ts
@@ -1,8 +1,28 @@
 import { DappyLookupOptions, ReturnCode } from '../../types';
+import { isObjectWith, isStringNotEmpty } from '../../utils/validation';
 import { dedent } from '../utils/dedent';
 import { Command } from './command';
 import { formatNetwork } from './lookupCommand';
-import { getNetwork } from './utils';
+import { getArgsMap, getNetwork } from './utils';
+
+export const isOutArgs = (
+  args: Record<string, string | boolean>,
+): args is {
+  out: string;
+} =>
+  isObjectWith({
+    out: isStringNotEmpty,
+  })(args);
+
+const getBaseFilename = (args: string[], name: string) => {
+  const argsMap = getArgsMap(args);
+
+  if (isOutArgs(argsMap)) {
+    return argsMap.out.replace(/\.pem$/, '');
+  }
+
+  return name;
+};
 
 export const saveCertificateCommand: Command = {
   description: dedent`
@@ -11,14 +31,27 @@ export const saveCertificateCommand: Command = {
     If multiple certificates are found, they are suffixed with a number.
 
     Positioned arguments:
-    1. name: <certificate name>
+      1. name: <certificate name>
 
     Optional arguments:
-    --out=<relative_path>
+      --out=<certificate_path>
+      --network=<network_id>
+      --endpoint=<http_url>
+      --hostname=<hostname>
+      --cacert=<ca_cert_file_path>
+      --network-file=<network_json_file_path>
+
+    Built-in network ids are:
+      - d
+      - gamma
 
     Examples:
-    
-      dappy-lookup save-certificate example
+      # Download example.dappy certificate(s) using dappy network (mainnet)
+      dappy-lookup savecertificate example.dappy 
+      # Download example.dappy certificate(s) using gamma network, save it/them to custom path
+      dappy-lookup savecertificate example.dappy --out=customname.pem --network=gamma
+      # Download example.dappy certificate(s) using a custom dappy-node network defined in a JSON config file
+      dappy-lookup savecertificate example.dappy --network-file=./custom-network.json
   `,
   action: async ([name, ...rest], api) => {
     if (!name) {
@@ -46,14 +79,16 @@ export const saveCertificateCommand: Command = {
       return 1;
     }
 
+    const baseFileName = getBaseFilename(rest, name);
+
     for (let i = 0; i < packet.answers.length; i += 1) {
       const answer = packet.answers[i];
-      const filename = `${answer.name}${
+      const filename = `${baseFileName}${
         packet.answers.length > 1 ? `-${i + 1}` : ''
       }.pem`;
       // eslint-disable-next-line no-await-in-loop
       await api.writeFile(filename, answer.data);
-      api.print(`Certificate ${name} found and saved to ./${filename}`);
+      api.print(`Certificate ${name} found and saved to ${filename}`);
     }
 
     return 0;
