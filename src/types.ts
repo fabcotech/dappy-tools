aa4660e2c061c44eae5a83c8b07587bc89520279
Paul Musso <paul@musso.io>
2022-03-29 15:18:57 +0200

    fix: type warnings

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,8 @@ import { DappyNetwork } from './model/DappyNetwork';
 export * from './model/NamePacket';
 export * from './model/DappyNetwork';
 export * from './model/NameZone';
+export * from './model/ResourceRecords';
+export * from './utils/json';
 
 export type DappyLookupOptions = {
   dappyNetwork: DappyNetwork;
