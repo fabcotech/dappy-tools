144060b0d2579821b8a42b3516b28665160ac2da
Fabco <contact@fabco.tech>
2022-03-30 16:00:48 +0200

    2.0.6

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,25 @@
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import typescript from "rollup-plugin-typescript";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      sourcemap: true,
-      format: "umd",
-      name: "Bees",
-      file: `dist/index.js`,
-    },
-  ],
-  plugins: [
-    typescript(),
-    resolve(),
-    commonjs(),
-  ],
-};
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+];
--- src/__tests__/resolver.spec.ts
+++ src/__tests__/resolver.spec.ts
@@ -1,4 +1,7 @@
-import { resolver, ResolverOutput, BeesLoadError }  from "../index";
+import chai, { expect } from 'chai';
+import spies from 'chai-spies';
+
+import { resolver, BeesLoadError }  from "../index";
 import { fakeQueryHandler } from "../fakeQueryHandler";
 
 export interface Query {
@@ -8,171 +11,130 @@ export interface Query {
   headers: { [key: string]: any };
 }
 
-const query: Query = {
-  path: "/listen-data-at-name",
-  parameters: {},
-  method: "POST",
-  headers: {}
-};
+chai.use(spies);
+
+const stringify = JSON.stringify
 
 describe("resolver, 1 node", () => {
-  it("should fail with invalid resolverAccuracy", done => {
-    resolver(fakeQueryHandler, ["https://nodea.org"], 30, 1)
-      .then((a: ResolverOutput) => {})
-      .catch(err => {
-        expect(err).toEqual("resolverAccuracy should be a number (percentage) between 50 and 100");
-        done()
-      })
+  it("should fail with invalid resolverAccuracy", async () => {
+    try {
+      await resolver(
+        fakeQueryHandler, ["https://nodea.org"], 30, 1
+      )
+    } catch (err) {
+      expect(err).equal("resolverAccuracy should be a number (percentage) between 50 and 100")
+    }    
   });
 
-  it("should fail with invalid resolverAbsolute", done => {
-    resolver(fakeQueryHandler, ["https://nodea.org"], 100, 2)
-      .then((a: ResolverOutput) => {})
-      .catch(err => {
-        expect(err).toEqual("resolverAbsolute should be an integer number and not exceed the length of ids");
-        done()
-      })
+  it("should fail with invalid resolverAbsolute", async () => {
+    try {
+      await resolver(fakeQueryHandler, ["https://nodea.org"], 100, 2)
+    } catch (err) {
+      expect(err).equal("resolverAbsolute should be an integer number and not exceed the length of ids");
+    }
   });
 
-  it("should complete with one available node 100/1", done => {
-    resolver(fakeQueryHandler, ["https://nodea.org"], 100, 1)
-      .then((a: ResolverOutput) => {
-        expect(a).toEqual({
-          loadErrors: {},
-          loadState: {
-            "1": {
-              ids: ["https://nodea.org"],
-              data: "a",
-              stringToCompare: "a"
-            }
-          },
-          loadPending: [],
-          status: "completed"
-        });
-        done();
-      });
+  it("should complete with one available node 100/1", async () => {
+
+    const a = await resolver(fakeQueryHandler, ["https://nodea.org"], 100, 1)
+    console.log(a)
+    expect(stringify(a)).equal(stringify({
+      loadErrors: {},
+      loadState: {
+        "1": {
+          ids: ["https://nodea.org"],
+          data: "a",
+          stringToCompare: "a"
+        }
+      },
+      loadPending: [],
+      status: "completed"
+    }))
   });
 
-  it("should fail OutOfNodes with unavailable node 100/1", done => {
-    resolver(fakeQueryHandler, ["https://nodefail.org"], 100, 1)
-    .then((a: ResolverOutput) => {
-        expect(a.loadErrors).toEqual({
-          "https://nodefail.org": {
-            id: "https://nodefail.org",
-            status: 400
-          }
-        });
-        expect(a.loadError).toEqual({
-          error: BeesLoadError.OutOfNodes,
-          args: {
-            alreadyQueried: 0,
-            resolverAbsolute: 1
-          }
-        });
-        expect(a.status).toBe("failed");
-        done();
-      
-    });
+  it("should fail OutOfNodes with unavailable node 100/1", async () => {
+    const a = await resolver(fakeQueryHandler, ["https://nodefail.org"], 100, 1);
+    expect(stringify(a.loadErrors)).equal(stringify({
+      "https://nodefail.org": {
+        id: "https://nodefail.org",
+        status: 400
+      }
+    }))
+    expect(stringify(a.loadError)).equal(stringify({
+      error: BeesLoadError.OutOfNodes,
+      args: {
+        alreadyQueried: 0,
+        resolverAbsolute: 1
+      }
+    }));
+    expect(a.status).equal("failed");
   });
 });
 
 describe("resolver, 2 nodes, 100/2", () => {
-  it("should complete with two nodes A", done => {
-    resolver(
+  it("should complete with two nodes A", async () => {
+    const a = await resolver(
       fakeQueryHandler,
       ["https://nodea1.org", "https://nodea2.org"],
       100,
       2
     )
-    .then((a: ResolverOutput) => {
-      expect(a).toEqual({
-        loadErrors: {},
-        loadState: {
-          "1": {
-            ids: ["https://nodea1.org", "https://nodea2.org"],
-            data: "a",
-            stringToCompare: "a"
-          }
-        },
-        loadPending: [],
-        status: "completed"
-      });
-      done();
-    });
+    expect(stringify(a)).equal(stringify({
+      loadErrors: {},
+      loadState: {
+        "1": {
+          ids: ["https://nodea1.org", "https://nodea2.org"],
+          data: "a",
+          stringToCompare: "a"
+        }
+      },
+      loadPending: [],
+      status: "completed"
+    }));
   });
 
-  it("should fail OutOfNodes with one node A and one node B", done => {
-    resolver(
+  it("should fail OutOfNodes with one node A and one node B", async () => {
+    const a = await resolver(
       fakeQueryHandler,
       ["https://nodea1.org", "https://nodeb1.org"],
       100,
       2
-    )
-    .then((a: ResolverOutput) => {
-      expect(a.loadState).toEqual({
-        "1": {
-          ids: ["https://nodea1.org"],
-          data: "a",
-          stringToCompare: "a"
-        },
-        "2": {
-          ids: ["https://nodeb1.org"],
-          data: "b",
-          stringToCompare: "b"
-        }
-      });
-      expect(a.loadError).toEqual({
-        error: BeesLoadError.OutOfNodes,
-        args: {
-          resolverAbsolute: 2,
-          alreadyQueried: 2
-        }
-      });
-      expect(a.status).toBe("failed");
-      done();
-    });
+    );
+    expect(stringify(a.loadState)).equal(stringify({
+      "1": {
+        ids: ["https://nodea1.org"],
+        data: "a",
+        stringToCompare: "a"
+      },
+      "2": {
+        ids: ["https://nodeb1.org"],
+        data: "b",
+        stringToCompare: "b"
+      }
+    }));
+    expect(stringify(a.loadError)).equal(stringify({
+      error: BeesLoadError.OutOfNodes,
+      args: {
+        alreadyQueried: 2,
+        resolverAbsolute: 2,
+      }
+    }));
+    expect(a.status).equal("failed");
+
   });
 });
 
 describe("resolver, 3 nodes", () => {
-  it("should complete 51/2 with two nodes A, one node B", done => {
-    resolver(
+  it("should complete 51/2 with two nodes A, one node B", async () => {
+    const a = await resolver(
       fakeQueryHandler,
       ["https://nodeb1.org", "https://nodea1.org", "https://nodea2.org"],
       51,
       2
-    )
-    .then((a: ResolverOutput) => {
-      expect(a).toEqual({
-        loadErrors: {},
-        loadState: {
-          "1": {
-            ids: ["https://nodeb1.org"],
-            data: "b",
-            stringToCompare: "b"
-          },
-          "2": {
-            ids: ["https://nodea1.org", "https://nodea2.org"],
-            data: "a",
-            stringToCompare: "a"
-          },
-        },
-        loadPending: [],
-        status: "completed"
-      });
-      done();
-    });
-  });
-
-  it("should fail UnaccurateState 67/2 with two nodes A, one node B", done => {
-    resolver(
-      fakeQueryHandler,
-      ["https://nodeb1.org", "https://nodea1.org", "https://nodea2.org"],
-      90,
-      2
-    )
-    .then((a: ResolverOutput) => {
-      expect(a.loadState).toEqual({
+    );
+    expect(stringify(a)).equal(stringify({
+      loadErrors: {},
+      loadState: {
         "1": {
           ids: ["https://nodeb1.org"],
           data: "b",
@@ -183,54 +145,73 @@ describe("resolver, 3 nodes", () => {
           data: "a",
           stringToCompare: "a"
         },
-      });
-      expect(a.loadError).toEqual({
-        error: BeesLoadError.UnaccurateState,
-        args: {
-          totalOkResponses: 3,
-          loadStates: [
-            {
-              key: "1",
-              okResponses: 1,
-              percent: 33.33
-            },
-            {
-              key: "2",
-              okResponses: 2,
-              percent: 66.67
-            },
-          ]
-        }
-      });
-      expect(a.status).toBe("failed");
-      done();
-    });
+      },
+      loadPending: [],
+      status: "completed"
+    }));
   });
 
-  it("should succeed with 100/2 with two nodes A, one node failing", done => {
-    resolver(
+  it("should fail UnaccurateState 67/2 with two nodes A, one node B", async () => {
+    const a = await resolver(
+      fakeQueryHandler,
+      ["https://nodeb1.org", "https://nodea1.org", "https://nodea2.org"],
+      90,
+      2
+    );
+    expect(stringify(a.loadState)).equal(stringify({
+      "1": {
+        ids: ["https://nodeb1.org"],
+        data: "b",
+        stringToCompare: "b"
+      },
+      "2": {
+        ids: ["https://nodea1.org", "https://nodea2.org"],
+        data: "a",
+        stringToCompare: "a"
+      },
+    }));
+    expect(stringify(a.loadError)).equal(stringify({
+      error: BeesLoadError.UnaccurateState,
+      args: {
+        totalOkResponses: 3,
+        loadStates: [
+          {
+            key: "1",
+            okResponses: 1,
+            percent: 33.33
+          },
+          {
+            key: "2",
+            okResponses: 2,
+            percent: 66.67
+          },
+        ]
+      }
+    }));
+    expect(a.status).equal("failed");
+  });
+
+  it("should succeed with 100/2 with two nodes A, one node failing", async () => {
+    const a = await resolver(
       fakeQueryHandler,
       ["https://nodea1.org", "https://nodea2.org", "https://nodefail.org"],
       100,
       2
     )
-    .then((a: ResolverOutput) => {
-      expect(a.status).toBe("completed");
-      expect(a.loadState).toEqual({
-        "1": {
-          ids: ["https://nodea1.org", "https://nodea2.org"],
-          data: "a",
-          stringToCompare: "a"
-        }
-      });
-      done();
-    });
+    expect(a.status).equal("completed");
+    expect(stringify(a.loadState)).equal(stringify({
+      "1": {
+        ids: ["https://nodea1.org", "https://nodea2.org"],
+        data: "a",
+        stringToCompare: "a"
+      }
+    }));
   });
 });
 
 describe("resolver, 10 nodes", () => {
-  it("should fail 91/10 with 9 nodes A, 1 node B, 1 node fails", done => {
-    resolver(
+  it("should fail 91/10 with 9 nodes A, 1 node B, 1 node fails", async () => {
+    const a = await resolver(
       fakeQueryHandler,
       [
         "https://nodea1.org", 
@@ -248,42 +229,39 @@ describe("resolver, 10 nodes", () => {
     91,
     10
     )
-    .then((a: ResolverOutput) => {
-      expect(a.loadState).toEqual({
-        "1": {
-          ids: [
-            "https://nodea1.org", 
-            "https://nodea2.org",
-            "https://nodea3.org",
-            "https://nodea4.org",
-            "https://nodea5.org",
-            "https://nodea6.org",
-            "https://nodea7.org",
-            "https://nodea8.org",
-            "https://nodea9.org",
-          ],
-          data: "a",
-          stringToCompare: "a"
-        },
-        "2": {
-          ids: ["https://nodeb1.org"],
-          data: "b",
-          stringToCompare: "b"
-        },
-      });
 
-      expect(a.loadError).toEqual({
-        error: BeesLoadError.OutOfNodes,
-        args: {
-          alreadyQueried: 10,
-          resolverAbsolute: 10,
-        }
-      });
+    expect(stringify(a.loadState)).equal(stringify({
+      "1": {
+        ids: [
+          "https://nodea1.org", 
+          "https://nodea2.org",
+          "https://nodea3.org",
+          "https://nodea4.org",
+          "https://nodea5.org",
+          "https://nodea6.org",
+          "https://nodea7.org",
+          "https://nodea8.org",
+          "https://nodea9.org",
+        ],
+        data: "a",
+        stringToCompare: "a"
+      },
+      "2": {
+        ids: ["https://nodeb1.org"],
+        data: "b",
+        stringToCompare: "b"
+      },
+    }));
 
-      done();
-    });
+    expect(stringify(a.loadError)).equal(stringify({
+      error: BeesLoadError.OutOfNodes,
+      args: {
+        alreadyQueried: 10,
+        resolverAbsolute: 10,
+      }
+    }));
   });
-  it("should succeed 91/10 with 10 nodes A, 2 nodes fail", done => {
+  it("should succeed 91/10 with 10 nodes A, 2 nodes fail", async () => {
     const nodes = [
       "https://nodea1.org", 
       "https://nodea2.org",
@@ -298,23 +276,19 @@ describe("resolver, 10 nodes", () => {
       "https://nodea9.org",
       "https://nodea10.org",
     ]
-    resolver(
+    const a = await resolver(
       fakeQueryHandler,
       nodes,
       91,
       10
     )
-    .then((a: ResolverOutput) => {
-      expect(a.status).toEqual("completed");
-      expect(a.loadState).toEqual({
-        "1": {
-          ids: nodes.filter(a => a.includes("nodea")),
-          data: "a",
-          stringToCompare: "a"
-        }
-      });
-
-      done();
-    });
+    expect(a.status).equal("completed");
+    expect(stringify(a.loadState)).equal(stringify({
+      "1": {
+        ids: nodes.filter(a => a.includes("nodea")),
+        data: "a",
+        stringToCompare: "a"
+      }
+    }));
   });
 });
