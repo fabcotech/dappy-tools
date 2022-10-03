362351748e4a9a0d15d6b4e1ac5c5b9c3e1c9d78
Paul Musso <paul@musso.io>
2022-10-03 16:53:06 +0200

    fix: remove async from nodeLookup

--- packages/dappy-lookup/src/nodeLookup/nodeLookup.ts
+++ packages/dappy-lookup/src/nodeLookup/nodeLookup.ts
@@ -6,34 +6,36 @@ import { RecordType } from '../model/ResourceRecords';
 
 export const nodeLookup =
   (lookupFn: typeof dappyLookupRecord) =>
-  async (
+  (
     name: string,
     options: dns.LookupOneOptions,
     callback: (...args: any[]) => void,
   ) => {
-    const packet = await lookupFn(
-      name,
-      options.family === 6 ? RecordType.AAAA : RecordType.A,
+    lookupFn(name, options.family === 6 ? RecordType.AAAA : RecordType.A).then(
+      (packet) => {
+        const family = options.family === 6 ? 6 : 4;
+
+        if (!packet.answers || !packet.answers.length) {
+          callback(
+            new Error(
+              `No address found for name ${name} (format: IPv${family})`,
+            ),
+          );
+          return;
+        }
+
+        const addresses = packet.answers.map(({ data }) => data);
+
+        if (!addresses || addresses.length === 0) {
+          callback(
+            new Error(
+              `No address found for name ${name} (format: IPv${options.family})`,
+            ),
+          );
+          return;
+        }
+
+        callback(null, addresses[0], family);
+      },
     );
-    const family = options.family === 6 ? 6 : 4;
-
-    if (!packet.answers || !packet.answers.length) {
-      callback(
-        new Error(`No address found for name ${name} (format: IPv${family})`),
-      );
-      return;
-    }
-
-    const addresses = packet.answers.map(({ data }) => data);
-
-    if (!addresses || addresses.length === 0) {
-      callback(
-        new Error(
-          `No address found for name ${name} (format: IPv${options.family})`,
-        ),
-      );
-      return;
-    }
-
-    callback(null, addresses[0], family);
   };
