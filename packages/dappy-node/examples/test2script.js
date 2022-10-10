ea9fd34ea8ddd1e1c4ce520d1b01238687e56b6a
Fabco <contact@fabco.tech>
2022-10-10 13:08:29 +0200

    docs(node): removal of the examples in dappy-node

--- packages/dappy-node/examples/test2script.js
+++
@@ -1,5 +0,0 @@
-const s = `-------\nCSP script-src ok !\nThis script hosted at address https://test2.dappy/test2script.js has been loaded from helloworld.dappy:3005 (or whatever is your current host)
-
-This is not blocked by Content Security Policy, everything is fine :)\n-------\n`;
-
-console.log(s);
